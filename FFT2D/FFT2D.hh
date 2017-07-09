9374141d82a31948b925a89dbaa89034ff6bc317
ROGER Nicolas <roger.nicolas45@gmail.com>
2017-07-09 15:30:19 +0200

    clean up / thread

--- FFT2D/FFT2D.hh
+++ FFT2D/FFT2D.hh
@@ -5,11 +5,16 @@
 #include <vector>
 #include <stdexcept>
 #include <cmath>
+#include <atomic>
+#include <thread>
+#include "image.hh"
 #define PI 3.141592653589793238460
+static std::atomic<int> thread_number(8);
 
 typedef std::complex<double> Complex;
 void RecursiveFFT(std::valarray<Complex*>& ComplexCoef);
-void FFT2D(std::valarray<std::valarray<Complex*>>& ComplexCoef);
-void FFT2DInverse(std::valarray<std::valarray<Complex*>>& ComplexCoef);
-void InverseFFT(std::valarray<Complex*>& ComplexCoef);
+void FFT2DInverse(im& image);
+void linesFFT2D(im& image);
+void linesFFT2DInverse(im& image);
+void FFT2D(im& image);
 size_t sup_pow_two(size_t n);
